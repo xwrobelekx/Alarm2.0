fa8b93e24910e08c46d12bbe9099373de0fa7281
Kamil Wrobel <wrobelk@live.com>
2018-08-28 17:38:55 -0600

    Implemented swipe to delete - tested

--- Alarm2.0/View Controller/AlarmListTableViewController.swift
+++ Alarm2.0/View Controller/AlarmListTableViewController.swift
@@ -55,17 +55,18 @@ class AlarmListTableViewController: UITableViewController {
     }
     
 
-    /*
+   
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            let alarmToDelete = AlarmController.shared.alarms[indexPath.row]
+            AlarmController.shared.delete(alarm: alarmToDelete)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+       
+        }
     }
-    */
+   
 
     /*
     // Override to support rearranging the table view.
