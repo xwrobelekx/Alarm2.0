1a2646e24f5aa5df7c7e37924c2ebcd748f3b8f4
Kamil Wrobel <wrobelk@live.com>
2018-08-28 19:06:46 -0600

    Wired up Detial Table View Controller, replaced incorect PickerView with DatePickerView, prepared updateView function

--- Alarm2.0/View Controller/AlarmDetailTableViewController.swift
+++ Alarm2.0/View Controller/AlarmDetailTableViewController.swift
@@ -12,9 +12,21 @@ class AlarmDetailTableViewController: UITableViewController {
     
     //MARK: - Outlets
     
-    @IBOutlet weak var datePicker: UIPickerView!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var titletextField: UITextField!
     @IBOutlet weak var eanbleButton: UIButton!
+    
+    //MARK: - Properties
+    // this property will hold an alarm if were updatinf one, or will be nil if were creating one
+    var alarmForUpdate : Alarm? {
+        didSet {
+            loadViewIfNeeded()
+            updateViews()
+        }
+    }
+    
+    var alarmIsOn : Bool = true // line 140 on README
     
     
 
@@ -23,7 +35,23 @@ class AlarmDetailTableViewController: UITableViewController {
 
     }
 
-  
+    //MARK: - Methods
+    private func updateViews() {
+        if alarmForUpdate != nil {
+        guard let alarm = alarmForUpdate else {return}
+        alarmIsOn = alarm.enabled
+        datePicker.date = alarm.fireDate
+        titletextField.text = alarm.name
+            
+        if alarmIsOn {
+        eanbleButton.setTitle("ON", for: .normal)
+            eanbleButton.backgroundColor = .green
+        } else {
+            eanbleButton.setTitle("OFF", for: .normal)
+            eanbleButton.backgroundColor = .gray
+        }
+        }
+    }
     
     
     //MARK: - Actions
